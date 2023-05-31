c361011a80941fc5ddbd8adbb5ac8d90a87f1e0a
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-05-31 19:36:41 +0500

    Updated code with minor fixes

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,23 +2,33 @@ import { useState } from 'react';
 import DigitsButtons from './DigitsButtons';
 import OperatorsButtons from './OperatorsButtons';
 import './Calculator.css';
-import calculate from './logic/calculate';
+import Calculate from './logic/calculate';
 
 export default function Calculator() {
-  const [state, setState] = useState({
+  const [displayValue, setDisplayValue] = useState('0');
+  const [calculatorData, setCalculatorData] = useState({
     total: null,
     next: null,
     operation: null,
   });
 
   const handleButtonClick = (buttonName) => {
-    const newData = calculate(state, buttonName);
-    setState(newData);
+    const newData = Calculate(calculatorData, buttonName);
+    setCalculatorData(newData);
+
+    // Update the display value based on the calculator data
+    let newDisplayValue = '0';
+    if (newData.next) {
+      newDisplayValue = newData.next;
+    } else if (newData.total) {
+      newDisplayValue = newData.total;
+    }
+    setDisplayValue(newDisplayValue);
   };
 
   return (
     <div className="calculator">
-      <div className="cal-display">{state.next || state.total || '0'}</div>
+      <div className="cal-display">{displayValue}</div>
       <div className="buttons-wrapper">
         <DigitsButtons handleButtonClick={handleButtonClick} />
         <OperatorsButtons handleButtonClick={handleButtonClick} />
