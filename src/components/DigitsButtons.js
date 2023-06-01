71282232b45321227f795716644a0450691f2cf9
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-06-01 21:02:11 +0500

    Sorted cal digits in a correct order

--- src/components/DigitsButtons.js
+++ src/components/DigitsButtons.js
@@ -6,15 +6,15 @@ const DigitsButtons = ({ handleButtonClick }) => (
     <Button label="AC" handleClick={handleButtonClick} />
     <Button label="+/-" handleClick={handleButtonClick} />
     <Button label="%" handleClick={handleButtonClick} />
-    <Button label="9" handleClick={handleButtonClick} />
-    <Button label="8" handleClick={handleButtonClick} />
     <Button label="7" handleClick={handleButtonClick} />
-    <Button label="6" handleClick={handleButtonClick} />
-    <Button label="5" handleClick={handleButtonClick} />
+    <Button label="8" handleClick={handleButtonClick} />
+    <Button label="9" handleClick={handleButtonClick} />
     <Button label="4" handleClick={handleButtonClick} />
-    <Button label="3" handleClick={handleButtonClick} />
-    <Button label="2" handleClick={handleButtonClick} />
+    <Button label="5" handleClick={handleButtonClick} />
+    <Button label="6" handleClick={handleButtonClick} />
     <Button label="1" handleClick={handleButtonClick} />
+    <Button label="2" handleClick={handleButtonClick} />
+    <Button label="3" handleClick={handleButtonClick} />
     <Button label="0" handleClick={handleButtonClick} />
     <Button label="." handleClick={handleButtonClick} />
   </div>
