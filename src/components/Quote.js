e981067e71c9282c549c1e32adfe4fdb8e48e27a
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-06-01 15:45:11 +0500

    Made quote.js api call async using useEffect hook

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -7,20 +7,22 @@ function Quote() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('https://api.api-ninjas.com/v1/quotes?category=knowledge', {
-      headers:
-        { 'X-Api-Key': 'A5gXbih2P+SXXKb1RLdvfw==7imgYlRKUhRp6KRD' },
-    })
-      .then((resp) => resp.json())
-      .then((apiData) => {
-        setLoading(false);
+    const fetchData = async () => {
+      try {
+        const resp = await fetch('https://api.api-ninjas.com/v1/quotes?category=knowledge', {
+          headers:
+            { 'X-Api-Key': 'A5gXbih2P+SXXKb1RLdvfw==7imgYlRKUhRp6KRD' },
+        });
+        const apiData = await resp.json();
         const body = `${apiData[0].quote} - ${apiData[0].author}`;
         setQuote(body);
-      })
-      .catch((error) => {
-        setLoading(false);
+      } catch (error) {
         setError(error);
-      });
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
   }, []);
 
   if (loading) {
