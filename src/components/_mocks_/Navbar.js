141a851d72e2a743d5d97fce8a351af8a7f2cad8
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-06-07 17:08:02 +0500

    Added mock navbar

---
+++ src/components/_mocks_/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, NavLink } from 'react-router-dom';
+
+const Navbar = [
+  { to: '/', text: 'Home' },
+  { to: '/calculator', text: 'Calculator' },
+  { to: '/quote', text: 'Quote' },
+];
+
+const Nav = () => (
+  <BrowserRouter>
+    <nav className="navbar">
+      <h1>~ Math Magician ~</h1>
+      <ul>
+        {Navbar.map((link) => (
+          <li key={link}>
+            <NavLink to={link.to} className="nav-link">{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  </BrowserRouter>
+);
+
+export default Nav;
